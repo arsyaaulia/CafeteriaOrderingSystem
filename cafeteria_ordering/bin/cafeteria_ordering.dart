bc84a6f055c497f59c9cf59746a4071d82aa119f
Arsya Aulia <arsya1522@gmail.com>
2025-09-11 07:04:56 +0800

    add cafeteria_ordering

---
+++ cafeteria_ordering/bin/cafeteria_ordering.dart
@@ -0,0 +1,5 @@
+import 'package:cafeteria_ordering/cafeteria_ordering.dart' as cafeteria_ordering;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cafeteria_ordering.calculate()}!');
+}
